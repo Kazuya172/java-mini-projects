8508440e7e517fa3b636fd380a77cc78227a3feb
Nicolas Zwing <kcalb172@gmail.com>
2026-01-15 18:13:33 -0300

    Initial commit: calculator project

---
+++ javapj/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Type a number: ");
+
+    if (!input.hasNextInt()) {
+        System.out.println("Please type a valid number");
+        return;
+    }
+
+    int numero1 = input.nextInt();
+
+    System.out.print("Type another number: ");
+
+    if(!input.hasNextInt()) {
+        System.out.println("Please type a valid number");
+        return;
+    }
+
+    int numero2 = input.nextInt();
+
+    System.out.print("Type the Mathematical Operators (+ - * /): ");
+    String operator = input.next();
+
+    if (!operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/")) {
+        System.out.print("Please type a valid operator (+ - * /)");
+    }
+
+
+    if (operator.equals("+")) {
+        System.out.print(numero1 + numero2);
+    }
+    else if (operator.equals("-")) {
+        System.out.print(numero1 - numero2);
+    }
+    else if (operator.equals("*")) {
+        System.out.print(numero1 * numero2);
+    }
+    else if (operator.equals("/")) {
+        System.out.print(numero1 / numero2);
+        }
+    input.close();
+}
